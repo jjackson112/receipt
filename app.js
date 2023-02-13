68519c0cfb8ac4c86f8c87bea0f5879b4b3c791b
J. Jackson <jjackson11211@gmail.com>
2023-02-13 16:54:57 -0500

    More JS

---
+++ app.js
@@ -0,0 +1,40 @@
+
+// Store data of the inputs
+
+const article = document.getElementById('item')
+const articleAmounts = document.getElementById('total')
+const calculateBtn = document.querySelector('button')
+
+let articles = [];
+let list = document.getElementById("#article-list");
+let amounts = [];
+
+// Clear forms after data is entered
+
+function clearForm() {
+    document.form.reset();
+}
+
+// Display articles and their amounts
+
+calculateBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const newArticle = document.getElementById('item').value;
+    const newAmount = document.getElementById('total').value;
+    
+    newArticle.forEach((article) => {
+        let li = document.createElement("li");
+        li.innerText = article;
+        newArticle.append(list);
+});
+
+
+// Calculate total amounts
+
+function calculateReceipt() {
+    const finalTotal = document.getElementById('#final-total');
+    let sum = 0;
+    amounts.forEach(number => sum += number);
+    finalTotal.innerHTML = `Total is ${finalTotal}`;
+}
